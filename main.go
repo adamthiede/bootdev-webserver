67a2efbe7d4b50faa264c07ad43929fbb9bd3785
Adam Thiede <me@adamthiede.com>
2024-06-05 20:44:00 -0500

    add api

--- main.go
+++ main.go
@@ -8,13 +8,43 @@ import (
 
 func main() {
 	fmt.Println("Serving the web.")
+
+	apiCfg := apiConfig{
+		fileserverHits: 0,
+	}
+
 	sm := http.NewServeMux()
 
+	// healthz
 	healthzHandler := func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "OK")
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(http.StatusText(http.StatusOK)))
 	}
-	sm.HandleFunc("/healthz", healthzHandler)
-	sm.Handle("/app/*", http.StripPrefix("/app/", http.FileServer(http.Dir("html"))))
+	sm.HandleFunc("GET /api/healthz", healthzHandler)
+
+	// metrics
+	metricsHandler := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, fmt.Sprintf("Hits: %v", apiCfg.fileserverHits))
+	}
+	sm.HandleFunc("GET /api/metrics", metricsHandler)
+
+	//reset
+	sm.HandleFunc("/api/reset", apiCfg.resetHandler)
+
+	// admin metrics
+	adminMetricsHandler := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", apiCfg.fileserverHits))
+	}
+	sm.HandleFunc("GET /admin/metrics", adminMetricsHandler)
+
+	// app
+	appHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir("html"))))
+	sm.Handle("/app/", appHandler)
 
 	server := http.Server{
 		Handler: sm,
